41f4e6f8e21ea522f17cbddbb3693524b496ff46
akh89 <akhilkhandelwal@ymail.com>
2017-12-02 20:34:03 -0500

    Initial project setup

---
+++ src/main/java/com/fullstacktut/FullstackTutApplication.java
@@ -0,0 +1,12 @@
+package com.fullstacktut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackTutApplication.class, args);
+	}
+}
